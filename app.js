81b3f09b288a12ffe2234e19c4d5672b05d719c3
Brent Jameson <brent@jamesonnetworks.com>
2014-05-10 10:59:00 -0400

    Fixing app to point to renamed server

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ child.stderr.on('data', function (data) { process.stdout.write(data.toString());
 child.on('error', function(err) { process.stdout.write(err); process.stdout.write(arguments); });
 
 process.chdir('../../server/');
-child = spawn('node', ['app.js']);
+child = spawn('node', ['journal_server.js']);
 child.stdout.on('data', function (data) { process.stdout.write(data.toString()); });
 child.stderr.on('data', function (data) { process.stdout.write(data.toString()); });
 child.on('error', function(err) { process.stdout.write(err); process.stdout.write(arguments); });
